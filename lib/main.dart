977890750538eae691d4e57ea76d61d4108550c4
LeonWPCoding <wyp09787@gmail.com>
2025-09-01 00:31:57 +0630

    main config

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:magus/features/authentication/authentication.dart';
-import 'package:magus/firebase_options.dart';
+  import 'package:firebase_core/firebase_core.dart';
+  import 'package:flutter/material.dart';
+  import 'package:magus/features/authentication/authentication.dart';
+  import 'package:magus/firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
-}
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
+    runApp(MyApp());
+  }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AuthRoute(), // This is where auth state is reflected
-    );
-  } 
-}
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: AuthRoute(), // This is where auth state is reflected
+      );
+    } 
+  }
