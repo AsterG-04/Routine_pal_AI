15d684fe99756a6e1b4ca96c2e17ed31763e9736
LeonWPCoding <wyp09787@gmail.com>
2025-08-31 23:15:24 +0630

    good

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:magus/features/authentication/authentication.dart';
+import 'package:magus/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
